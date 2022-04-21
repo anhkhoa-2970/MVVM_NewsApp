a1d7a3b04abff3d9a7d0a47d44678d1a5d4932e7
Khoa Nguyen <1851120021@sv.ut.edu.vn>
2022-04-21 17:40:39 +0700

    add bottom_navigation_menu.xml and run

---
+++ app/src/main/java/com/example/mvvm_newsapp/ui/fragments/SavedNewsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.mvvm_newsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.mvvm_newsapp.R
+
+class SavedNewsFragment: Fragment(R.layout.fragment_breakingnews) {
+}
